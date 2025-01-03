02393debc88bc73396d18920959fc0138518f07d
marefboombeach@gmail.com <imareef>
2025-01-03 13:19:24 +0330

    Data Bindings using INotifyPropertyChanged

---
+++ Data Bindings using INotifyPropertyChanged/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Data_Bindings_using_INotifyPropertyChanged
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
