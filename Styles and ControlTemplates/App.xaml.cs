ccba7e9647a0e91307572575268ad7b83c48f413
marefboombeach@gmail.com <imareef>
2025-01-03 22:51:29 +0330

    Styles and ControlTemplates

---
+++ Styles and ControlTemplates/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Styles_and_ControlTemplates
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
