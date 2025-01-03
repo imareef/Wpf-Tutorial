c885b9bdea6686bdb91bc5f91a56b6781f1dccd1
marefboombeach@gmail.com <imareef>
2025-01-03 15:03:04 +0330

    List View

---
+++ ListView/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            lvEntries.Items.Add(tbEntry.Text); 
+            tbEntry.Text = string.Empty;
+
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            //int index = lvEntries.SelectedIndex;  selectionMode = single 
+            //lvEntries.Items.RemoveAt(index);
+
+            var items = lvEntries.SelectedItems; 
+            
+            var itemList = new ArrayList(items);
+            foreach (var item in itemList)
+                lvEntries.Items.Remove(item);
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            lvEntries.Items.Clear();
+        }
+    }
+}
