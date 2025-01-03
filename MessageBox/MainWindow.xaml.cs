f93f11e9b97f0ca10f5ee0c9193b1f596d1493d2
marefboombeach@gmail.com <imareef>
2025-01-03 13:37:24 +0330

    Message Box

---
+++ MessageBox/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace MessageBoxLearn
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFire_Click(object sender, RoutedEventArgs e)
+        {
+           
+            MessageBoxResult result = MessageBox.Show("Do You Agree?", "Agreement", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                tbInfo.Text = "agreed";
+            }
+            else
+            {
+                tbInfo.Text = "not agreed";
+            }
+
+        }
+    }
+}
