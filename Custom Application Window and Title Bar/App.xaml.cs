97e20347f9c1f62d43decb5533e99945864cf202
marefboombeach@gmail.com <imareef>
2025-01-03 19:35:39 +0330

    Custom Application Window and Title Bar

---
+++ Custom Application Window and Title Bar/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Custom_Application_Window_and_Title_Bar
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
